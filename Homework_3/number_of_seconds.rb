14f98af67ae05d6c9a1ee07fb2b275cabc6715a3
Michael Haney <michael.haney@colorado.edu>
2016-02-24 18:36:06 -0700

    Homework_3
    [#114450479]

---
+++ Homework_3/number_of_seconds.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+puts "There are 60 seconds in a minute"
+puts "There are 60 minutes in an hour"
+puts "There are 24 hours in a day"
+puts "There are 7 days in a week"
+puts "That means there are:"
+number_of_seconds_per_minute = 60
+number_of_minutes_per_hour = 60
+number_of_hours_per_day = 24
+number_of_days_per_week = 7
+number_of_weeks_per_year = 52
+young = 20
+old = 100
+puts "#{number_of_minutes_per_hour * number_of_seconds_per_minute} seconds in an hour,"
+puts "#{number_of_hours_per_day * number_of_minutes_per_hour * number_of_seconds_per_minute} seconds in a day,"
+puts "#{number_of_days_per_week * number_of_hours_per_day * number_of_minutes_per_hour * number_of_seconds_per_minute} seconds in a week"
+puts "That means when you are 20, you've been alive for #{young * number_of_weeks_per_year * number_of_days_per_week * number_of_hours_per_day * number_of_minutes_per_hour * number_of_seconds_per_minute} seconds, and if you make it to 100, you will have lived #{old * number_of_weeks_per_year * number_of_days_per_week * number_of_hours_per_day * number_of_minutes_per_hour * number_of_seconds_per_minute} seconds. Make them count!"
+
