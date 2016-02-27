55d3e7e089027c1485e2a8defb20b54cbca30a2e
Michael Haney <michael.haney@colorado.edu>
2016-02-26 18:06:49 -0700

    Completed 1st homework of methods and flow control.
    [#114579917]

---
+++ methods_and_flow_control/string_practice.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+base = 'this string is to practice with'
+puts base
+puts base.capitalize
+puts base.upcase
+puts base.capitalize.sub('string', "'string'")
+puts "The string '" + base + "' has #{base.length + 2} characters"
+puts base.reverse
+puts "Practice " * 2 + "Practice!"
