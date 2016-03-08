5f5e5a6085787377b24f5330585053d9fc905467
Michael Haney <michael.haney@colorado.edu>
2016-03-08 13:32:49 -0700

    Cash Register initial commit [#114739805]

---
+++ objects/cash_register.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+# This is the CashRegister class
+class CashRegister
+  attr_accessor :purchase, :total, :pay
+
+  def initialize
+    @total = 0.0
+  end
+
+  def purchase(number)
+    @total += number
+  end
+
+  def pay(number)
+    @total -= number
+    if @total > 0.0
+      puts "Your new total is #{@total}"
+    else
+      puts "Your change is $#{@total * -1}"
+      @total = 0.0
+    end
+  end
+end
